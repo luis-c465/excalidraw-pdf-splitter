6f86fb310e8377366c72cb2e3ceca4e1c6994d8b
Luis Canada <canadaluis5@gmail.com>
2024-01-24 23:00:43 -0500

    Add the little logo

--- src/components/HomeScreen.tsx
+++ src/components/HomeScreen.tsx
@@ -5,6 +5,7 @@ import ExcalidrawDemo from "./ExcalidrawDemo";
 import FileDrop from "./FileDrop";
 import FileLoader from "./FileLoader";
 import PDFDemo from "./PDFDemo";
+import logo from "/logo.png?url";
 
 export default function HomeScreen() {
   const [resizedImage] = useAtomValue(resizedImageAtom);
@@ -23,9 +24,12 @@ export default function HomeScreen() {
 function TitleBlock() {
   return (
     <div className="flex flex-col gap-5">
-      <h1 className="inline-block bg-gradient-to-r from-indigo-400 to-red-400 bg-clip-text text-9xl text-transparent">
-        ExcaliDraw to PDF
-      </h1>
+      <div className="flex gap-2">
+        <img className="text-9xl h-[1em]" src={logo} />
+        <h1 className="inline-block bg-gradient-to-r from-indigo-400 to-red-400 bg-clip-text text-9xl text-transparent">
+          ExcaliDraw to PDF
+        </h1>
+      </div>
 
       <span className="text-muted-foreground text-xl">
         Converts sections of an excalidraw file to a PDF
